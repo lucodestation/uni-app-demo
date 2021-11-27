484298da58d1edc4eaab3efa2df9a78d61714008
卢忠伟 <lucodestation@163.com>
2021-11-27 23:31:42 +0800

    VSCode 格式化

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import Vue from 'vue'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-    ...App
+  ...App,
 })
 app.$mount()
 // #endif
@@ -15,7 +15,7 @@ import { createSSRApp } from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
   return {
-    app
+    app,
   }
 }
 // #endif
