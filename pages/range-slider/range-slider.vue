1c75d4ad575285f91c57a8009613c8bb77f4ac92
卢忠伟 <lucodestation@163.com>
2021-12-10 00:13:31 +0800

    滑块加上手写气泡提示文字（不完美）

--- pages/range-slider/range-slider.vue
+++ pages/range-slider/range-slider.vue
@@ -1,22 +1,22 @@
 <template>
   <view>
-    <view>滑块</view>
-    <view class="range-slider-wrapper">
-      <!-- <range-slider /> -->
+    <view>
+      <button @click="handleButton">按钮</button>
+    </view>
 
-      <slider-range
-        :value="rangeValue"
-        :min="rangeMin"
-        :max="rangMax"
-        :step="5"
-        :bar-height="3"
-        :block-size="26"
-        background-color="#f00"
-        active-color="#080"
-        :format="format"
-        :decorationVisible="true"
-        @change="handleRangeChange"
-      ></slider-range>
+    <view class="range-slider-wrapper">
+      <range-slider
+        width="690"
+        height="50"
+        blockSize="20"
+        activeColor="#FC7411"
+        min="0"
+        max="100"
+        :values="rangeValue"
+        :bubbleText="bubbleText"
+        @rangechange="rangeChange"
+      >
+      </range-slider>
     </view>
   </view>
 </template>
@@ -26,17 +26,41 @@ export default {
   data() {
     return {
       rangeValue: [30, 50],
-      rangeMin: 0,
-      rangMax: 100,
+      bubbleText: '30-40万',
     }
   },
-  methods: {},
+  methods: {
+    rangeChange(event) {
+      // console.info(event)
+      const value = [event.minValue * 1, event.maxValue * 1]
+      this.rangeValue = value
+      console.log(value)
+      if (value[0] === value[1] && value[0] !== 0 && value[0] !== 100) {
+        this.bubbleText = value[0] + '万'
+      } else if (
+        value[0] === value[1] &&
+        (value[1] === 100 || value[1] === 0)
+      ) {
+        this.bubbleText = '不限'
+      } else if (value[0] === 0 && value[1] === 100) {
+        this.bubbleText = '不限'
+      } else if (value[1] === 100) {
+        this.bubbleText = value[0] + '万以上'
+      } else {
+        this.bubbleText = value[0] + '-' + value[1] + '万'
+      }
+    },
+    handleButton() {
+      console.info('button')
+      this.rangeValue = [20, 60]
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .range-slider-wrapper {
   padding: 30rpx;
-  background-color: #eee;
+  background-color: #080;
 }
 </style>
