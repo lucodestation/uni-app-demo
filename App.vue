4a5884a980fa44ad0e4fa8e820d6f5124231be13
卢忠伟 <lucodestation@163.com>
2023-11-09 18:48:11 +0800

    忽略node_modules，调整VConsole调用位置

--- App.vue
+++ App.vue
@@ -1,15 +1,39 @@
 <script>
+// #ifdef H5
+import VConsole from "vconsole";
+// #endif
+
 export default {
+  globalData: {
+    // #ifdef H5
+    vConsole: null,
+    // #endif
+  },
   onLaunch() {
-    console.log('App Launch')
+    // #ifdef H5
+    if (
+      location.hostname.startsWith("localhost") ||
+      location.hostname.startsWith("127.0.0.1") ||
+      location.hostname.startsWith("192.168.")
+    ) {
+      this.vConsole = new VConsole();
+    }
+    // #endif
+    console.log("App Launch");
   },
   onShow() {
-    console.log('App Show')
+    console.log("App Show");
   },
   onHide() {
-    console.log('App Hide')
+    console.log("App Hide");
   },
-}
+  onExit() {
+    // #ifdef H5
+    this.vConsole.destroy();
+    // #endif
+    console.log("App Exit");
+  },
+};
 </script>
 
 <style>
--- main.js
+++ main.js
@@ -3,17 +3,6 @@ import App from "./App";
 // #ifndef VUE3
 import Vue from "vue";
 
-// #ifdef H5
-import VConsole from "vconsole";
-if (
-  location.hostname.startsWith("localhost") ||
-  location.hostname.startsWith("127.0.0.1") ||
-  location.hostname.startsWith("192.168.")
-) {
-  new VConsole();
-}
-// #endif
-
 Vue.config.productionTip = false;
 App.mpType = "app";
 const app = new Vue({
